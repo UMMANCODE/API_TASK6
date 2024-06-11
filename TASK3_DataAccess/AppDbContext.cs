ce3c43d11760cd0ae9e595c39f6a22ad9da8e172
UMMANCODE <ummanmm@code.edu.az>
2024-06-12 00:35:31 +0400

    Files added

---
+++ TASK3_DataAccess/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Azure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using TASK3_Core.Entities;
+
+namespace TASK3_DataAccess {
+  public class AppDbContext : DbContext {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<Group> Groups { get; set; }
+    public DbSet<Student> Students { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder) {
+      base.OnModelCreating(builder);
+      builder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+  }
+}
+
